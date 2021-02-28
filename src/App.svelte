36a466d9afd7553cfcfa556d3c8c5b651f85452b
Qbius <mich@lsurowka.email>
2021-02-28 18:10:43 +0100

    Reverting to default

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,5 @@
 <script>
-
-	const range = n => [...Array(n).keys()];
-
-	const materials = {
-		dust: range(6).map(i => 24272 + i),
-		sac: range(6).map(i => 24278 + i),
-		scale: range(6).map(i => 24284 + i),
-		blood: range(6).map(i => 24290 + i),
-		totem: [].concat(range(4).map(i => 24272 + i), [24363], [24300]),
-		claw: range(6).map(i => 24346 + i),
-		fang: range(6).map(i => 24352 + i),
-		bone: [].concat(range(4).map(i => 24272 + i), [24341], [24358]),
-	}
-
-all_ids_str = ','.join(chain(*[list(map(str, ids)) for ids in fines.values()]))
-payload = loads(urlopen('https://api.guildwars2.com/v2/commerce/prices?ids=' + all_ids_str).read())
-info = {priceinfo['id']: (priceinfo['buys']['unit_price'], priceinfo['sells']['unit_price']) for priceinfo in payload}
-
-def compare(tier, typ):
-    if tier < 2 or tier > 6: return "Invalid tier"
-    index = tier - 1
-
-    print(loads(urlopen(f'https://api.guildwars2.com/v2/items/{fines[typ][index]}').read())['name'])
-    print(fines[typ][index])
-    sells_for, buys_for = info[fines[typ][index]]
-    sells_for_prev, _ = info[fines[typ][index - 1]]
-    sells_for_dust, _ = info[fines['dust'][index]]
-
-    cost = sells_for + (5 * sells_for_dust) + (50 * sells_for_prev)
-    payoff_min = (6.91 if tier == 6 else 18.51) * buys_for
-    payoff_avg = (6.91 if tier == 6 else 18.51) * buys_for
-    return (, (6.91 if tier == 6 else 18.51) * buys_for)
-
-print(compare(3, 'bone'))
+	export let name;
 </script>
 
 <main>
