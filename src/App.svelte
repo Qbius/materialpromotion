b0bfec367835d47b6fdec079eb69873b26c96caa
Qbius <mich@lsurowka.email>
2021-02-28 22:35:13 +0100

    0.0.1

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,17 @@
 <script>
-	import {localhost, range} from './common.js';
-
-	const materials = {
-		dust: range(6).map(i => 24272 + i),
-		sac: range(6).map(i => 24278 + i),
-		scale: range(6).map(i => 24284 + i),
-		blood: range(6).map(i => 24290 + i),
-		totem: [].concat(range(4).map(i => 24296 + i), [24363], [24300]),
-		claw: range(6).map(i => 24346 + i),
-		fang: range(6).map(i => 24352 + i),
-		bone: [].concat(range(4).map(i => 24342 + i), [24341], [24358]),
-	};
+	import * as example from './materials_example.json';
+	import {localhost, materials} from './common.js';
+	import Matable from './Matable.svelte';
 
 	async function get_tp_info() {
-		if (localhost) return [];
+		if (localhost) return example.default;
 		
 		const response = await fetch('/api/tpinfo?ids=' + [].concat(...Object.values(materials)).join(','));
 		if (response.ok) {
 			const json = await response.json();
 			console.log(json);
 			console.log(JSON.stringify(json));
-			return Object.entries(json);
+			return json;
 		}
 		else
 			throw new Error('Error loading trading post info');
@@ -31,9 +22,11 @@
 	{#await get_tp_info()}
 		<h1>Loading...</h1>
 	{:then tpinfo}
-		{#each tpinfo as info}
+		<!-- {#each tpinfo as info}
 			<h1>{info[0]}: {info[1][0]} {info[1][1]}</h1>
-		{/each}
+		{/each} -->
+		<Matable {tpinfo}/>
+
 	{:catch error}
 		<h1>{error}</h1>
 	{/await}
@@ -42,9 +35,13 @@
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
 		max-width: 240px;
-		margin: 0 auto;
+
+		width: 100%;
+		height: 100%;
+
+		display: flex;
+		justify-content: center;
 	}
 
 	h1 {
--- src/common.js
+++ src/common.js
@@ -1,2 +1,81 @@
 export const range = n => [...Array(n).keys()];
 export const localhost = location.hostname === "localhost";
+export const materials = {
+    blood: range(6).map(i => 24290 + i),
+    bone: [].concat(range(4).map(i => 24342 + i), [24341], [24358]),
+    claw: range(6).map(i => 24346 + i),
+    dust: range(6).map(i => 24272 + i),
+    fang: range(6).map(i => 24352 + i),
+    scale: range(6).map(i => 24284 + i),
+    totem: [].concat(range(4).map(i => 24296 + i), [24363], [24300]),
+    sac: range(6).map(i => 24278 + i),
+};
+
+export const material_names = {
+    blood: [
+        'Vial of Weak Blood',
+        'Vial of Thin Blood',
+        'Vial of Blood',
+        'Vial of Thick Blood',
+        'Vial of Potent Blood',
+        'Vial of Powerful Blood',
+    ],
+    bone: [
+        'Bone Chip',
+        'Bone Shard',
+        'Bone',
+        'Heavy Bone',
+        'Large Bone',
+        'Ancient Bone',
+    ],
+    claw: [
+        'Tiny Claw',
+        'Small Claw',
+        'Claw',
+        'Sharp Claw',
+        'Large Claw',
+        'Vicious Claw',
+    ],
+    dust: [
+        'Pile of Glittering Dust',
+        'Pile of Shimmering Dust',
+        'Pile of Radiant Dust',
+        'Pile of Luminous Dust',
+        'Pile of Incandescent Dust',
+        'Pile of Crystalline Dust',
+    ],
+    fang: [
+        
+        'Tiny Fang',
+        'Small Fang',
+        'Fang',
+        'Sharp Fang',
+        'Large Fang',
+        'Vicious Fang',
+    ],
+    scale: [
+        
+        'Tiny Scale',
+        'Small Scale',
+        'Scale',
+        'Smooth Scale',
+        'Large Scale',
+        'Armored Scale',
+    ],
+    totem: [
+        'Tiny Totem',
+        'Small Totem',
+        'Totem',
+        'Intricate Totem',
+        'Engraved Totem',
+        'Elaborate Totem',
+    ],
+    sac: [      
+        'Tiny Venom Sac',
+        'Small Venom Sac',
+        'Venom Sac',
+        'Full Venom Sac',
+        'Potent Venom Sac',
+        'Powerful Venom Sac',
+    ],
+};
